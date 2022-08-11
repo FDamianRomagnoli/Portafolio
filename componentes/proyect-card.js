cbc161857f814caa1ab14f5a979ed9132f54b191
FDamianRomagnoli <fdamianromagnoli@gmail.com>
2022-08-11 20:37:00 -0300

    v1.0.4

--- componentes/proyect-card.js
+++ componentes/proyect-card.js
@@ -21,12 +21,13 @@ class proyectCard extends HTMLElement {
             <article class="proyect-card">
 
                 <div class="proyect-card__front">
+                    <div class="card-opacity"></div>
                     <h2>${this.title}</h2>
                 </div>
 
                 <div class="proyect-card__description">
                     <p>${this.description}</p>
-                    <button class="proyect-card__button" onclick="visitarProyecto('${this.url}')">Visitar</button>
+                    <button class="proyect-card__button" onclick="visitarProyecto('${this.url}')">VISITAR</button>
                 </div>
 
             </article>
@@ -49,32 +50,122 @@ class proyectCard extends HTMLElement {
         }
 
         :host{
-            max-width:300px;
+            display: flex;
+            justify-content: center;
         }
 
 
         .proyect-card{
             width: 100%;
-            height: 420px;
+            max-width: 320px;
+            height: 380px;
             background: #FFFFFF;
             box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
             border-radius: 10px;
-            display: flex;
-            flex-direction: column;
         }
 
         .proyect-card__front{
+            position: relative;
             height: 45%;
             background: black;
             border-top-left-radius: 10px;
             border-top-right-radius: 10px;
+            background-image: url('${this.image}');
+            background-repeat: no-repeat;
+            background-size: cover;
+            box-shadow: 0px 1px 4px #000000;
+        }
+
+        .card-opacity{
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            z-index: 1;
+            background: rgba(0,0,0,.5);
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+        }
+
+        .proyect-card__front h2{
+            font-style: normal;
+            font-weight: 500;
+            text-shadow: 1px 1px 2px #FFF;
+            line-height: 28px;
+            position: relative;
+            text-align: center;
+            top: calc( 50% - 10px );
+            color: #FFFFFF;
+            font-size: 20px;
+            z-index: 2;
         }
         
         .proyect-card__description{
-            height: 60%;
+            position: relative;
+            height: 55%;
             border-bottom-left-radius: 10px;
             border-bottom-right-radius: 10px;
+            padding: 20px;
         }
+
+        .proyect-card__description p{
+            font-size: 14px;
+            font-weight: 500;
+            color: #000;
+        }
+
+        .proyect-card__button{
+            position: absolute;
+            left: calc( 50% - 75px );
+            bottom: 10px;
+            background: #0E0E0E;
+            border-radius: 20px;
+            outline: none;
+            height: 35px;
+            width: 150px;
+            border: none;
+            color: #FFFFFF;
+            box-shadow: 0px 0px 4px 0px #000;
+            font-family: 'Raleway';
+            font-size: 15px;
+            font-weight: 700;
+        }
+
+        @media (min-width: 310px){
+            .proyect-card{
+                height: calc( 80vw * 1.5 );
+                max-height: 490px;
+            }
+        }
+
+        @media (min-width: 330px){
+
+            .proyect-card__description p{
+                font-size: 16px;
+            }
+        }
+
+        @media (min-width: 370px){
+
+            .proyect-card__description p{
+                font-size: 18px;
+            }
+
+            .proyect-card__front h2{
+                position: relative;
+                text-align: center;
+                top: calc( 50% - 15px );
+                color: #FFFFFF;
+                font-size: 25px;
+            }            
+        }
+
+        @media (min-width: 700px){
+            :host{
+                display: inline-block;
+                max-width: 320px;
+            }
+        }
+
 
         `;
     }
