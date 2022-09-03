b1da44541bb100c0bb5b8b676c65e6134863af9d
FDamianRomagnoli <fdamianromagnoli@gmail.com>
2022-09-03 02:41:58 -0300

    v2.1.0

--- componentes/proyect-card.js
+++ componentes/proyect-card.js
@@ -1,35 +1,44 @@
 class proyectCard extends HTMLElement {
+
     constructor(){
         super();
         this.attachShadow({mode: "open"});
     }
 
     static get observedAttributes() {
-        return ["title","description","image","url"];
+        return ["nombre","imagen","descripcion","produccion","herramientas"];
     }
 
-    attributeChangedCallback(atributo, valorAntiguo, valorNuevo) {
-        if (valorAntiguo !== valorNuevo) {
-            this[atributo] = valorNuevo;
+    attributeChangedCallback(attr, oldVal, newVal) {
+        if (attr === "nombre") {
+          this.nombre = newVal;
         }
-    }
+        if (attr === "imagen") {
+          this.imagen = newVal;
+        }
+        if (attr === "descripcion") {
+          this.descripcion = newVal;
+        }
+        if (attr === "produccion") {
+          this.produccion = newVal;
+        }
+        if (attr === "herramientas") {
+          this.herramientas = newVal;
+        }
+        this.disconnectedCallback();
+        this.connectedCallback();
+      }
 
     getTemplate(){
         const template = document.createElement("TEMPLATE");
 
         template.innerHTML = `
             <article class="proyect-card">
-
-                <div class="proyect-card__front">
-                    <div class="card-opacity"></div>
-                    <h2>${this.title}</h2>
-                </div>
-
-                <div class="proyect-card__description">
-                    <p>${this.description}</p>
-                    <button class="proyect-card__button" onclick="visitarProyecto('${this.url}')">VISITAR</button>
-                </div>
-
+                <h3>${this.nombre}</h3>
+                <div></div>
+                <p class="herramientas"><b>Tecnologías utilizadas: </b>${this.herramientas}</p>
+                <p class="produccion"><b>Producido en: </b>${this.produccion}</p>
+                <p class="descripcion"><b>Descripción: </b>${this.descripcion}</p>
             </article>
 
             <style>
@@ -42,146 +51,38 @@ class proyectCard extends HTMLElement {
 
     getStyle(){
         return `
-
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing:border-box;
-        }
-
-        :host{
-            display: flex;
-            justify-content: center;
-        }
-
-
-        .proyect-card{
-            width: 100%;
-            max-width: 320px;
-            height: 380px;
-            background: #FFFFFF;
-            box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-            border-radius: 10px;
-        }
-
-        .proyect-card__front{
-            position: relative;
-            height: 45%;
-            background: black;
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-            background-image: url('${this.image}');
-            background-repeat: no-repeat;
-            background-size: cover;
-            box-shadow: 0px 1px 4px #000000;
-        }
-
-        .card-opacity{
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            z-index: 1;
-            background: rgba(0,0,0,.5);
-            border-top-left-radius: 10px;
-            border-top-right-radius: 10px;
-        }
-
-        .proyect-card__front h2{
-            font-style: normal;
-            font-weight: 500;
-            text-shadow: 1px 1px 2px #FFF;
-            line-height: 28px;
-            position: relative;
-            text-align: center;
-            top: calc( 50% - 10px );
-            color: #FFFFFF;
-            font-size: 20px;
-            z-index: 2;
-        }
-        
-        .proyect-card__description{
-            position: relative;
-            height: 55%;
-            border-bottom-left-radius: 10px;
-            border-bottom-right-radius: 10px;
-            padding: 20px;
-        }
-
-        .proyect-card__description p{
-            font-size: 14px;
-            font-weight: 500;
-            color: #000;
-        }
-
-        .proyect-card__button{
-            position: absolute;
-            left: calc( 50% - 75px );
-            bottom: 10px;
-            background: #111;
-            border-radius: 20px;
-            outline: none;
-            height: 35px;
-            width: 150px;
-            border: none;
-            color: #FFFFFF;
-            box-shadow: 0px 0px 4px 0px #000;
-            font-family: 'Raleway';
-            font-size: 15px;
-            font-weight: 700;
-        }
-
-        .proyect-card__button:hover{
-            cursor: pointer;
-        }
-
-        @media (min-width: 310px){
             .proyect-card{
-                height: calc( 80vw * 1.5 );
-                max-height: 490px;
-            }
-        }
-
-        @media (min-width: 330px){
-
-            .proyect-card__description p{
-                font-size: 16px;
-            }
-        }
-
-        @media (min-width: 370px){
-
-            .proyect-card__description p{
-                font-size: 18px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
             }
 
-            .proyect-card__front h2{
-                position: relative;
+            div{
+                background-image: url(${this.imagen});
+                background-repeat: no-repeat;
+                background-size: cover;
+                background-position: center;
+                height: 215px;
+                max-width: 350px;
+                border: 2px solid #000;
+                margin: 20px 0;
+                filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.7));
+                border-radius: 5px;
+            }
+
+            .proyect-card h3{
+                margin: 0 auto;
+                width: fit-content;
                 text-align: center;
-                top: calc( 50% - 15px );
-                color: #FFFFFF;
-                font-size: 25px;
-            }            
-        }
-
-        @media (min-width: 700px){
-            :host{
-                display: inline-block;
-                width: 320px;
-            }
-        }
-        @media (min-width: 1000px){
-            :host{
-                width: 400px;
-                height: 600px;
+                font-size: 36px;
+                font-weight: 300;
+                border-bottom: 1px solid #000; 
             }
 
-            .proyect-card{
-                max-height: 600px;
-                max-width: 400px;
+            p{
+                font-size: 15px;
             }
-        }
-
-        `;
+        `
     }
 
     render(){
@@ -190,6 +91,10 @@ class proyectCard extends HTMLElement {
 
     connectedCallback() {
         this.render();
+    }
+
+    disconnectedCallback() {
+        this.shadowRoot.innerHTML = "";
     }
 
 }
--- js/controllerProyectos.js
+++ js/controllerProyectos.js
@@ -17,22 +17,34 @@ function selectCard(num){
             return response.json();
         })
         .then(jsondata => {
+
             tamanioJSON = jsondata.length;
             counter.innerHTML =  num + " de " + tamanioJSON;
+            updateCard(jsondata[num-1]);
 
             btnAtras.addEventListener("click", () => {
                 num = num == 1 ? 1 : num - 1;
                 counter.innerHTML =  num + " de " + tamanioJSON;
+                updateCard(jsondata[num-1]);
             })
         
             btnSiguiente.addEventListener("click", () => {
                 num = num < tamanioJSON ? num + 1 : num;
                 counter.innerHTML =  num + " de " + tamanioJSON;
+                updateCard(jsondata[num-1]);
             })
         });
 
 }
 
+function updateCard(obj){
+    let card = document.querySelector("proyect-card");
+    card.setAttribute("nombre",obj["name"]);
+    card.setAttribute("imagen",obj["url"]);
+    card.setAttribute("descripcion",obj["descripcion"]);
+    card.setAttribute("produccion",obj["produccion"]);
+    card.setAttribute("herramientas",obj["herramientas"]);
 
+}
 
 main();
